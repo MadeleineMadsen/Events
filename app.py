866f47fc57e3c7b3ab32f3bd620a84a38d46be06
Madeleine Madsen <mama0002@stud.kea.dk>
2025-04-08 09:00:17 +0200

    Full demo øvelse

---
+++ app.py
@@ -0,0 +1,100 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+import x
+
+from icecream import ic
+ic.configureOutput(prefix=f'***** | ', includeContext=True)
+
+
+##############################
+@app.get("/")
+def show_index():
+    try:
+        db, cursor = x.db()
+        q = "SELECT * FROM items ORDER BY item_pk LIMIT %s OFFSET %s"
+        cursor.execute(q, (2, 0))
+        rows = cursor.fetchall()
+        ic(rows)
+        return render_template("page_index.html", title="Fleas 4 You", rows=rows)
+    except Exception as ex:
+        ic(ex)
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()
+
+##############################
+@app.get("/signup")
+def show_signup():
+    try:
+        return render_template("page_signup.html", title="Fleas Signup", x=x)
+    except Exception as ex:
+        ic(ex)
+    finally:
+        pass
+
+##############################
+@app.get("/login")
+def show_login():
+    try:
+        return render_template("page_login.html", title="Fleas Login", x=x)
+    except Exception as ex:
+        ic(ex)
+    finally:
+        pass
+
+
+##############################
+@app.get("/items/page/<page_number>")
+def get_items_by_page(page_number):
+    try:
+        # Validate - make sure that the page number is a digit/number
+        page_number = x.validate_page_number(page_number)
+        next_page = page_number + 1  
+        limit = 2
+        temp_limit = limit + 1 
+        offset = (int(page_number) - 1) * limit
+        db, cursor = x.db()
+
+        q = "SELECT * FROM items ORDER BY item_pk LIMIT %s OFFSET %s"
+        cursor.execute(q, (temp_limit, offset))
+        rows = cursor.fetchall()
+        ic(len(rows))
+
+        if(len(rows)) < 3:
+            button_html = """<mixhtml mix-remove="#button_get_more_items"></mixhtml>"""
+
+        else:
+            button = render_template("_button_get_more_items.html", next_page=next_page)
+            button_html = f"""
+                <mixhtml mix-update="#button_get_more_items">
+                    {button}
+                </mixhtml>
+                """
+            
+        final_html = ""
+        for row in rows[:2]:
+            item_html = render_template("_item.html", row=row)
+            final_html += item_html
+
+        return f"""
+            <mixhtml mix-bottom="#items">
+                {final_html}
+            </mixhtml>
+            { button_html }
+            """
+    
+    except Exception as ex:
+        ic(ex)
+        if "page number not valid" in str(ex): return "wrong page", 400
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()
+
+
+
+
+
+
+
+
+
---
+++ static/app.js
@@ -0,0 +1,11 @@
+var map = L.map('map').setView([55.68723172888712, 12.568084883486426], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+
+
+
