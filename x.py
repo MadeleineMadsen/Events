52e33d5de7a46cb0d663130ead0765584a4441d4
Madeleine Madsen <mama0002@stud.kea.dk>
2025-04-08 09:13:08 +0200

    Rettelse i x.py

--- x.py
+++ x.py
@@ -17,7 +17,7 @@ def db():
         host = "mysql",      # Replace with your MySQL server's address or docker service name "mysql"
         user = "root",  # Replace with your MySQL username
         password = "password",  # Replace with your MySQL password
-        database = "company"   # Replace with your MySQL database name
+        database = "events"   # Replace with your MySQL database name
     )
     cursor = db.cursor(dictionary=True)
     return db, cursor
